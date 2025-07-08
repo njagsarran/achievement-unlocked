99b2efc3898d733bb308dbbc04a420d20b7abebe
Nicholas Jagsarran <nikjagsarran@gmail.com>
2025-07-08 00:27:59 -0400

    Fade in and out

--- frontend/src/screens/UnlockAchievement/UnlockAchievement.js
+++ frontend/src/screens/UnlockAchievement/UnlockAchievement.js
@@ -12,16 +12,34 @@ const UnlockAchievement = () => {
   const [achievementVisible, setAchievementVisible] = useState(false);
   const [currentAchievement, setCurrentAchievement] = useState();
 
+  function getCSSVarTimeMs(name) {
+    const stringValue = getComputedStyle(document.documentElement).getPropertyValue(name).trim();
+    const floatValue = parseFloat(stringValue);
+    return floatValue * (stringValue.includes('ms') ? 1 : 1000);
+  }
+
+  const fadeInTime = getCSSVarTimeMs('--fade-in-time');
+  const holdTime = getCSSVarTimeMs('--hold-time');
+  const fadeOutTime = getCSSVarTimeMs('--fade-out-time');
+  
+  const totalDuration = fadeInTime + holdTime + fadeOutTime;
+
+  const showAchievement = () => {
+    const randomIndex = Math.floor(Math.random() * achievements.length);
+    setCurrentAchievement(achievements[randomIndex]);
+    setAchievementVisible(true);
+
+    setTimeout(() => {
+      setAchievementVisible(false);
+    }, totalDuration);
+  }
+
   return (
     <div className={styles.wrapper}>
       <div style={{ padding: 10 }} />
       <Button
         label="Unlock Achievement"
-        onClick={() => {
-          const randomIndex = Math.floor(Math.random() * achievements.length);
-          setCurrentAchievement(achievements[randomIndex]);
-          setAchievementVisible(true);
-        }}
+        onClick={showAchievement}
       />
       <div style={{ marginTop: '20px' }} />
       
